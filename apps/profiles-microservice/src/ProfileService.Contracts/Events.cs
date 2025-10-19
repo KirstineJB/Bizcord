21244ae42a38cf95638d08587371a9444620352f
KirstineJB <kirstine.berantzino@gmail.com>
2025-10-19 15:25:50 +0200

    add shared data contracts and event contracts  + application mapping

---
+++ apps/profiles-microservice/src/ProfileService.Contracts/Events.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileService.Contracts
+{
+    public record ProfileCreated(
+     Guid ProfileId,
+     string Username,
+     string DisplayName,
+     string Email,
+     DateTimeOffset CreatedAt
+ );
+
+    public record ProfileUpdated(
+        Guid ProfileId,
+        string DisplayName,
+        string Email,
+        DateTimeOffset UpdatedAt
+    );
+}
---
+++ apps/profiles-microservice/src/ProfileService.Contracts/profileSharedDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileService.Contracts
+{
+    /// <summary>
+    /// Public-facing for user profiler for inter-service communication.
+    /// Minimal fields; íngen internal details or behavior.
+    /// </summary>
+    public record ProfileSharedDto(
+        Guid Id,
+        string Username,
+        string DisplayName,
+        string Email,
+        DateTimeOffset CreatedAt
+
+    );
+}
