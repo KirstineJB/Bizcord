19878bc5ee130870a88a7ab7d78b4bd006a33b26
KirstineJB <kirstine.berantzino@gmail.com>
2025-10-19 18:58:10 +0200

    - add unit tests for Profile domain and application layers - implement integration tests for ProfileService API

---
+++ apps/profiles-microservice/tests/ProfileService.IntegrationTests/Support/ProfilesApiFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using ProfileService.Api.Messaging;
+using ProfileService.IntegrationTests.TestDoubles;
+
+namespace ProfileService.IntegrationTests.Support;
+
+public class ProfilesApiFactory : WebApplicationFactory<Program>
+{
+    public FakeMessageClient FakeBus { get; } = new();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder) 
+    {
+        builder.ConfigureServices(services =>
+        {
+           
+            var existing = services.SingleOrDefault(s => s.ServiceType == typeof(IMessageClient));
+            if (existing is not null) services.Remove(existing);
+            services.AddSingleton<IMessageClient>(FakeBus);
+        });
+    }
+}
