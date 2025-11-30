e09a58659b5c970def2a33cbd968f0d0e6285d56
KirstineJB <kirstine.berantzino@gmail.com>
2025-11-30 13:47:44 +0100

    - Added JWT for Authorization and authentication -Created a small token generator project for testing - Get profiles endpoint now requires specific role

--- apps/api-gateway/Program.cs
+++ apps/api-gateway/Program.cs
@@ -1,15 +1,58 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Loader Ocelot konfiguration
+
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-// Registrer Ocelot services
+
+JsonWebTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+
+var authenticationScheme = "Bearer";
+var issuer = "bizcord-auth";
+var audience = "bizcord-api";
+var secretKey = "SuperDuperMegaHemmeligTestNøgle";
+
+builder.Services
+    .AddAuthentication(authenticationScheme)
+    .AddJwtBearer(authenticationScheme, options =>
+    {
+        options.RequireHttpsMetadata = false; 
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = issuer,
+
+            ValidateAudience = true,
+            ValidAudience = audience,
+
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+
+            ValidateLifetime = false,
+
+            NameClaimType = "name",
+            RoleClaimType = "role"
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 await app.UseOcelot();
 
