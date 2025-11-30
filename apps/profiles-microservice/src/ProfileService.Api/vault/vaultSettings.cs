4ad0d18c66573e79fe3b2b318247105d234c7208
KirstineJB <kirstine.berantzino@gmail.com>
2025-11-30 18:15:28 +0100

    Reapply "-Implemented east/west security on endpoints"
    This reverts commit e3aec5177c137d15c855ab52173ae676d95047db.

---
+++ apps/profiles-microservice/src/ProfileService.Api/vault/vaultSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ProfileService.Api.vault
+{
+    public class VaultSettings
+    {
+        public string Address { get; set; } = "http://localhost:8200";
+        public string Token { get; set; } = "";
+        public string MountPoint { get; set; } = "secret";
+        public string RabbitMqSecretPath { get; set; } = "profile-service-mq";
+    }
+}
